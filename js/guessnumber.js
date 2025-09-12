abf80bbdeeda68893d9591e418c582a00a8ca186
malthe-kea <mati0002@stud.kea.dk>
2025-09-12 20:04:09 +0200

    init commit

---
+++ js/guessnumber.js
@@ -0,0 +1,11 @@
+console.log("jeg er i guessnumber")
+
+const lblMessage = document.querySelector(".message");
+console.log(lblMessage);
+console.log(lblMessage.textContent)
+
+const lblNumber = document.querySelector(".number");
+console.log(lblNumber);
+
+const lblScore = document.querySelector(".score");
+console.log(lblScore);
