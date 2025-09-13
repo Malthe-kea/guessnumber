fc1fc1cbcdf5c2f25a7fd149ed645fc46b79013e
malthe-kea <mati0002@stud.kea.dk>
2025-09-13 20:29:13 +0200

    Virker evfter hensigten

--- js/guessnumber.js
+++ js/guessnumber.js
@@ -1,11 +1,49 @@
 console.log("jeg er i guessnumber")
 
-const lblMessage = document.querySelector(".message");
+let lblMessage = document.querySelector(".message");
 console.log(lblMessage);
 console.log(lblMessage.textContent)
 
 const lblNumber = document.querySelector(".number");
 console.log(lblNumber);
 
-const lblScore = document.querySelector(".score");
+let lblScore = document.querySelector(".score");
 console.log(lblScore);
+
+const inpGuess = document.querySelector(".guess")
+console.log(inpGuess);
+
+const btnCheck = document.querySelector(".btn.check");
+console.log(btnCheck);
+
+const secretNumber = Math.trunc(Math.random() * 20);
+console.log(secretNumber);
+
+let startScore = 20;
+
+
+function testNumber() {
+    console.log(secretNumber);
+    console.log("testNumber()");
+    guess = Number(inpGuess.value);
+    console.log(inpGuess.value);
+    if (!guess) {
+        lblMessage.textContent = '⛔ No Number';
+    } else if (guess === secretNumber) {
+        lblMessage.textContent = '🎉 Du har gættet tallet';
+        console.log(lblMessage.textContent)
+    } else if (guess > secretNumber) {
+        lblMessage.textContent = "📈 for højt - prøv igen";
+        startScore--;
+        lblScore.textContent = startScore;
+        console.log(startScore)
+    } else if (guess < secretNumber) {
+        lblMessage.textContent = "📉 for lavt - prøv igen";
+        startScore--;
+        lblScore.textContent = startScore;
+        console.log(startScore)
+    }
+}
+
+btnCheck.addEventListener("click", testNumber);
+
